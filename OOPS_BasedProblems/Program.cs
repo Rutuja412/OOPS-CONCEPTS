ca22be197db2a264cc429337226421e1d8c57664
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-15 17:46:50 +0530

    [Rutuja]:Adding Programm For Inheritance

--- OOPS_BasedProblems/Program.cs
+++ OOPS_BasedProblems/Program.cs
@@ -7,8 +7,24 @@ namespace OOPS_BasedProblems
         static void Main(string[] args)
         {
             Console.WriteLine("OOPS_Concepts");
-            ClassAndObjects objects = new ClassAndObjects();
-            objects.CreateClassAndObject();
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.Class &Objects \n2.Inheritance(Vehicle)\n3.Exit ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1: Console.WriteLine("Class & Objects");
+                    ClassAndObjects objects = new ClassAndObjects();
+                    objects.CreateClassAndObject();
+                    break;
+                    case 2: Console.WriteLine("Inheritance ..( Ex.Vehicle)");
+                    Car car = new Car();
+                    car.horn();
+                    Console.WriteLine("Color:"+car.color +" \nBrand:" + car.brand);
+                    break;
+
+            }
+
+
         }
     }
 }
---
+++ OOPS_BasedProblems/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_BasedProblems
+{
+    class Vehicle
+    {
+        public string brand = "Ford";
+        public void horn()
+        {
+            Console.WriteLine("Peek Peek");
+        }
+    }
+    class Car : Vehicle
+    {
+        public string color = "Red";
+
+    }
+}
