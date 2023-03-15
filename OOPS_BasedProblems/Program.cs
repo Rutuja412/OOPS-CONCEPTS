62cf9f001956125a9b509357043d4bd998fdee43
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-15 23:15:24 +0530

    [Rutuja]:Adding Programm For Variables

--- OOPS_BasedProblems/Program.cs
+++ OOPS_BasedProblems/Program.cs
@@ -8,7 +8,7 @@ namespace OOPS_BasedProblems
         {
             Console.WriteLine("OOPS_Concepts");
             Console.WriteLine("Select given options");
-            Console.WriteLine("1.Class &Objects \n2.Inheritance(Vehicle)\n3.Polymorphism - Method Overloding\n4.Overriding(poly)\n5. Encapsulation \n6.Abstraction ");
+            Console.WriteLine("1.Class &Objects \n2.Inheritance(Vehicle)\n3.Polymorphism - Method Overloding\n4.Overriding(poly)\n5. Encapsulation \n6.Abstraction \n7.Variables");
             int option = Convert.ToInt16(Console.ReadLine());
             switch (option)
             {
@@ -41,6 +41,21 @@ namespace OOPS_BasedProblems
                     Cat cat = new Cat();    
                     cat.AnimalSound();
                     cat.Eat();
+                    break;
+                    case 7: Console.WriteLine("Variables Types");
+                    Variables variables = new Variables();
+                    variables.EmployeeAge();//Local
+                    Variables.Salary = 10000;//Static
+                    Console.WriteLine("Employee Salary =" + Variables.Salary);
+                    Console.WriteLine("Employee Salary ; " + Variables.Monthlysalary);//Const variable
+                    Console.WriteLine("Employee LastName;" + variables.LastName);//readomnly
+                    variables.EmployeeName = "Rutuja";//Instance 
+                    variables.EmployeeId = 1;
+                    Console.WriteLine("Employee Name= " + variables.EmployeeName+ " \nEmployee ID; "+ variables.EmployeeId);
+                    Variables variables1 = new Variables();
+                    variables1.EmployeeName = "Radha";//Instance Second Object
+                    variables1.EmployeeId = 2;
+                    Console.WriteLine("Employee Name= " + variables1.EmployeeName + " \nEmployee ID; " + variables1.EmployeeId);
                     break;
 
 
