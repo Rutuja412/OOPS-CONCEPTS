f7d073005e0929a996c4c951e714daaf4ac9d269
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-17 19:59:30 +0530

    [Rutuja]:Adding program for Interface

---
+++ OOPS_BasedProblems/IShape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_BasedProblems
+{
+    public interface IShape
+    {
+        void Draw();
+    }
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Draw Circle!!!");
+        }
+    }
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Draw Rectangle");
+        }
+    }
+}
